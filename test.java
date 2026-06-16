a6f8d2107af94b059df5ab37225cac08457a995c
onlinerui <45354165+onlinerui@users.noreply.github.com>
2026-06-16 14:30:10 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,8 @@
-com.ocbc.YjKE4X2F_W
 f2.com.ocbc.
 f3.com.ocbc.
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 控制台输出 Hello World
+        System.out.println("com.ocbc.YjKE4X2F_W");
+    }
+}
